c752e333140d9282c238ad609ff5875c76cb7981
flodardev <63950579+flodardev@users.noreply.github.com>
2020-07-09 21:01:16 +0800

    import utils, edit index.js

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hi, my name is Radolf. I'm a freelance developer.</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section>
       <h2 className={utilStyles.headingLg}>Blog</h2>
